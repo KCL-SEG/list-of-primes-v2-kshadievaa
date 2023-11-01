567bb3091e997830e0e7bb997aac241b558571ec
Kamila Shadieva <kamila.shadieva@kcl.ac.uk>
2023-11-01 02:14:36 +0000

    "Solve the list of primes"

--- primes.py
+++ primes.py
@@ -2,5 +2,17 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes <= 0:
+        raise ValueError(f"number_of_primes = {number_of_primes} must be a positive number")
+    else:
+        list = []
+        i = 2
+        while len(list) < number_of_primes:
+                isPrime = True
+                for j in range (2, i):
+                    if i % j == 0:
+                        isPrime = False
+                if isPrime:
+                    list.append(i)
+                i += 1
+        return list
